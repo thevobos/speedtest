f169cff84e0a46aa19019bb4e98b11157b3cc1a2
Cengiz AKCAN <thevobos@gmail.com>
2020-11-18 23:47:43 +0300

    Update speed.js

--- bin/speed.js
+++ bin/speed.js
@@ -7,7 +7,7 @@ var _config = require("./config.json");
 var opts = {
     url: [],
     "limits": _config.limits,
-    "port": process.env.ST_PORT || _config.port || 8080,
+    "port": process.env.PORT || 5000,
     "ip": process.env.ST_ADDR || _config.ip || "0.0.0.0"
 };
 
